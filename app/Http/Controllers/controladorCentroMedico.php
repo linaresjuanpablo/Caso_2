d0b7b45918b9be7ff587aef47ddac17ace82a140
Cristhian Salazar <cmsn_2007@hotmail.com>
2017-03-10 15:25:14 -0500

    merge en el controlador

--- app/Http/Controllers/controladorCentroMedico.php
+++ app/Http/Controllers/controladorCentroMedico.php
@@ -6,17 +6,37 @@ use Illuminate\Http\Request;
 
 class controladorCentroMedico extends Controller
 {
-  public function prueba()
-  {
-     return view('prueba');
-  }
+     function prueba(){
+        return view('prueba');
+    }
+
+  function gestionCitas(){
+        return view('citas');
+    }
+
+    function asignarCitas(){
+        return view('asignarCita');
+    }
+
   public function GestionPaciente()
   {
     return view('paciente/GestionPaciente');
   }
+
   public function Login()
   {
     return view('login/Login');
   }
+
+ public function pacientes()
+  {
+    return view('paciente/pacientes');
+  }
+
+  public function menuAdministrador()
+  {
+    return view('menuAdmin');
+  }
+
 }
 
