435973c810ac248f0142a2d9e42e8417fbf41ef1
ERVINETO <ervineto@gmail.com>
2017-03-23 03:56:08 -0500

    Se crea el modelo de Patient

---
+++ app/Patient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'documento','tipo_documento','nombres','apellidos', 'sexo', 'fecha_nacimiento', 'eps', 'telefono', 'direccion', 'nombres_acudiente', 'telefono_acudiente',
+    ];
+}
