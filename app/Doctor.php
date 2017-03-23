577780e20695df8a69d5d3dff5d3fe0497e0c483
juan_linares <linares.0@hotmail.com>
2017-03-23 18:57:43 -0500

    Se realiza el modelo doctor

---
+++ app/Doctor.php
@@ -0,0 +1,14 @@
+<?php
+
+ namespace App;
+
+ use Illuminate\Database\Eloquent\Model;
+
+    class Doctor extends Model
+    {
+     //
+        protected $fillable = [
+             'documento','tipo_documento','nombres','apellidos', 'especialidad', 'email', 'telefono',
+         ];
+        }
+    }
