9019040bb9d5a5632e001ac99a0a72a236931c00
juan_linares <linares.0@hotmail.com>
2017-03-24 16:46:49 -0500

    creacion crud doctor

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -38,6 +38,70 @@ class DoctorController extends Controller
        $doctors = Doctor::all();
        return view('doctors/doctorManager',['list' => $doctors]);
    }
+
+    public function show(Request $request, $id)
+    {
+
+    }
+
+     public function edit(Request $request, $id)
+    {
+        try
+        {
+            $doctor = Doctor::findOrFail($id);
+            return view('doctors/doctorEdit', ['data' => $doctor]);
+        }
+        catch(ModelNotFoundException $e)
+        {
+            Session::flash('flash_message', "El Medico ($id) no ha sido encontrado para editarlo!");
+            return redirect()->back();
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try
+        {
+            $doctor = Doctor::findOrFail($id);
+            $this->validate($request, [
+               'documento' => 'required | string | max:15',
+               'tipo_documento' => 'required | string | max:2',
+               'nombres' => 'required | string | max:100',
+               'apellidos' => 'required | string | max:100',
+               'especialidad' => 'required | string | max:50',
+               'email' => 'required | string | max:50',
+               'telefono' => 'required | string | max:50',
+               ]);
+            $input = $request->all();
+            $doctor->fill($input)->save();
+            Session::flash('flash_message', 'Medico editado correctamente!');
+            $doctors = Doctor::all();
+            return view('doctors/doctorManager',['list' => $doctors]);
+        }
+        catch(ModelNotFoundException $e)
+        {
+            Session::flash('flash_message', "El MEdico ($id) no ha sido encontrado para editarlo!");
+            echo "error";
+            return redirect()->back();
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        try
+        {
+            $doctor = Doctor::findOrFail($id);
+            $doctor->delete();
+            Session::flash('flash_message', 'Medico eliminado correctamente!');
+            $doctors = Doctor::all();
+            return view('doctors/doctorManager',['list' => $doctors]);
+        }
+        catch(ModelNotFoundException $e)
+        {
+            Session::flash('flash_message', "El Medico ($id) no ha sido encontrado para eliminarlo!");
+            return redirect()->back();
+        }
+    }
 }
 
 
---
+++ resources/views/doctors/doctorEdit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.layout2')
+@section('contenido')
+<div class="container-fluid">
+    <section class="row" >
+    <br>
+    <div>
+        <h2>Editar datos del Medico</h2>
+    </div>
+    <br>
+    </section>
+    <section class="row" >
+        <section class="col-md-4">
+            <br><br>
+            <img src={{ asset('images/medico.jpg') }}  class="img-responsive" alt="Responsive image">
+        </section>
+        <section class="col-md-8">
+            @if($errors->any())
+            <div class="alert alert-danger">
+                @foreach($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+            @endif
+            <div class="panel panel-default">
+                <div class="panel-heading"><h3 class="panel-title">Formulario de edición de Medico</h3></div>
+                    <div class="panel-body">
+                        {!! Form::model($data, ['method' => 'PATCH','route' => ['doctors.update', $data->id]])  !!}
+                        <div class="form-group">
+                            {!! Form::label('lblDocumento', 'Número de Documento:', ['class' => 'control-label']) !!}
+                            {!! Form::text('documento', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblTipoDocumento', 'Tipo de Documento:', ['class' => 'control-label']) !!}
+                            {!! Form::text('tipo_documento', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblNombres', 'Nombres:', ['class' => 'control-label']) !!}
+                            {!! Form::text('nombres', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblApellidos', 'Apellidos:', ['class' => 'control-label']) !!}
+                            {!! Form::text('apellidos', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblEspecialidad', 'Especialidad:', ['class' => 'control-label']) !!}
+                            {!! Form::text('especialidad', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblEmail', 'Email:', ['class' => 'control-label']) !!}
+                            {!! Form::text('email', null, ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('lblTelefono', 'Teléfono:', ['class' => 'control-label']) !!}
+                            {!! Form::text('telefono', null, ['class' => 'form-control']) !!}
+                        </div>
+                        {!! Form::submit('Guardar datos', ['class' => 'btn btn-primary']) !!}
+                        {!! Form::close() !!}
+                    </div>
+            </div>
+        </section>
+@stop
